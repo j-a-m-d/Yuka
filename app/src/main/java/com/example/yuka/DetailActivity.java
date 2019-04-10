7709fef26aad78dacce59e09c92fff50c2cd363d
ashorter <ashley.n.shorter@gmail.com>
2019-04-10 18:29:07 -0400

    no message

--- app/src/main/java/com/example/yuka/DetailActivity.java
+++ app/src/main/java/com/example/yuka/DetailActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.example.yuka.models.Location;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;

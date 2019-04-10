abbaf91a25b5f5f24551cec88016c98367368714
dwint321 <dwint321@gmail.com>
2019-04-09 23:37:47 -0400

    Adding the logout button and more fragment work

--- app/src/main/java/com/example/yuka/MainActivity.java
+++ app/src/main/java/com/example/yuka/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.yuka;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.example.yuka.fragments.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,28 +19,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
         bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
                 switch (item.getItemId()) {
                     case R.id.action_search:
-                        // lets get this bread(toast)... for now.
-                        Toast.makeText(MainActivity.this, "Search!", Toast.LENGTH_SHORT).show();
+                        fragment = new ProfileFragment();
+                        //Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_favorites:
-                        // lets get this bread(toast)... for now.
-                        Toast.makeText(MainActivity.this, "Favorites!", Toast.LENGTH_SHORT).show();
+                        fragment = new ProfileFragment();
+                        //Toast.makeText(MainActivity.this, "Compose!!!", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_profile:
-                        // lets get this bread(toast)... for now.
-                        Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
+                    default:
+                        fragment = new ProfileFragment();
+                        //Toast.makeText(MainActivity.this, "Profile!!!", Toast.LENGTH_SHORT).show();
                         break;
                 }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
         });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_profile);
     }
 }
 
---
+++ app/src/main/java/com/example/yuka/fragments/ProfileFragment.java
@@ -0,0 +1,65 @@
+package com.example.yuka.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.parse.ParseUser;
+import com.example.yuka.LoginActivity;
+import com.example.yuka.R;
+
+public class ProfileFragment extends Fragment {
+
+    private final String TAG = "ProfileFragment";
+
+    private ImageView ivProfilePic;
+    private TextView tvHandle;
+    private Button btnRecentSearch;
+    private Button btnLogout;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ivProfilePic = view.findViewById(R.id.ivProfilePic);
+        tvHandle = view.findViewById(R.id.tvHandle);
+        btnRecentSearch = view.findViewById(R.id.btnRecentSearch);
+        btnLogout = view.findViewById(R.id.btnLogout);
+        //view the user name
+
+        //put log out button here
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+                goLogin();
+            }
+        });
+    }
+
+    private void goLogin() {
+        Log.d(TAG, "Navigating to the Login Activity");
+        Intent i= new Intent(getContext(), LoginActivity.class);
+        startActivity(i);
+    }
+
+}
+
+
+
